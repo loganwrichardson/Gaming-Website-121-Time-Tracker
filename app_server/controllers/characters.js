93b721df269c56a8da4d72dddaf8194bce97bb34
loganwrichardson <richardsonlw@appstate.edu>
2024-04-29 16:57:24 -0400

    Migrated character info into the controllers

--- app_server/controllers/characters.js
+++ app_server/controllers/characters.js
@@ -1,6 +1,40 @@
 /* GET Character List */
 const characterlist = (req, res) => {
-    res.render('character-list', {title: 'Character List'});
+    res.render('character-list', {
+        title: 'Character List',
+        pageHeader: {
+            title: '1 to 1 Time Tracker',
+            strapline: 'Track your game characters!'
+        },
+        pcList: [{
+            name: 'Gandalf',
+            lockoutStatus: 'Locked Out',
+            level: 'Level 20',
+            stats: ['Body 10', 'Mind 16', 'Soul 20', 'HP 60'],
+            // body: 'Body 10',
+            // mind: 'Mind 16',
+            // soul: 'Soul 20',
+            // hp: 'HP 60'
+        }, {
+            name: 'Optimus Prime',
+            lockoutStatus: 'Available',
+            level: 'Level 18',
+            stats: ['Body 16', 'Mind 14', 'Soul 10', 'HP 100'],
+            // body: 'Body 16',
+            // mind: 'Mind 14',
+            // soul: 'Soul 10',
+            // hp: 'HP 100'
+        }, {
+            name: 'Eric Carman',
+            lockoutStatus: 'Available',
+            level: 'Level 08',
+            stats: ['Body 05', 'Mind 12', 'Soul 14', 'HP 84'],
+            // body: 'Body 05',
+            // mind: 'Mind 12',
+            // soul: 'Soul 14',
+            // hp: 'HP 84'
+        }]
+    });
 };
 
 /* GET 'Character info' page */
