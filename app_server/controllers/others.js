b58ef1b4e749cfdc25ec26ae8f1cd1896731ef93
loganwrichardson <richardsonlw@appstate.edu>
2024-04-29 21:28:52 -0400

    Moved character info into the controllers

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -2,7 +2,7 @@
 const about = (req, res) => {
     res.render('about', 
         {
-            title: 'About 1 to 1 TimeTracker',
+            title: 'About 1 to 1 Time Tracker',
             aboutContent: '1-to-1 Time Tracker was created by Taylor Fernald and Logan Richardson. \n\n\
             This website showcases what we have learned in Dr. Fenwick\'s grad MEAN web Dev.',
         }
